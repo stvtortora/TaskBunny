3c4ce1dfcaba073eb8f45d578bc5aa7d311b2276
Steven <stvtortora@gmail.com>
2018-05-18 10:45:05 -0400

    prepare for heroku push

--- frontend/components/taskers_form/taskers_form.jsx
+++ frontend/components/taskers_form/taskers_form.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Tasker from './Tasker';
+import Tasker from './tasker';
 import NavBar from '../nav_bar/nav_bar';
 import Modal from './modal';
 import { connect } from 'react-redux';
