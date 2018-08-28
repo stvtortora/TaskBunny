f1bf8aaf91b0ca360b72a05ea7d77c133179cdfc
Steven <stvtortora@gmail.com>
2018-08-28 00:50:14 -0400

    add puts statements for heroku logs

--- app/controllers/api/time_slots_controller.rb
+++ app/controllers/api/time_slots_controller.rb
@@ -1,11 +1,13 @@
 class Api::TimeSlotsController < ApplicationController
   def index
     if current_user.is_a? Client
+      puts 'tasker time slots being rendered'
     @time_slots = TimeSlot.includes(:hour, :day)
                           .joins(:time_slot_registrations)
                           .where('time_slot_registrations.filled = ?', false)
                           .where('time_slot_registrations.tasker_id = ?', params[:tasker_id])
     else
+      puts 'all time slots being rendered'
       @time_slots = TimeSlot.all
     end
 
