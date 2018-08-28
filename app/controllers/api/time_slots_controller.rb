5c4f8c4b56e3f3ce06e41245cdb51ae87d806486
Steven <stvtortora@gmail.com>
2018-08-28 01:25:02 -0400

    modify time slots controller

--- app/controllers/api/time_slots_controller.rb
+++ app/controllers/api/time_slots_controller.rb
@@ -1,16 +1,22 @@
 class Api::TimeSlotsController < ApplicationController
   def index
-    if current_user.is_a? Client
-      puts 'tasker time slots being rendered'
     @time_slots = TimeSlot.includes(:hour, :day)
-                          .joins(:time_slot_registrations)
-                          .where('time_slot_registrations.filled = ?', false)
-                          .where('time_slot_registrations.tasker_id = ?', params[:tasker_id])
-    else
-      puts 'all time slots being rendered'
-      @time_slots = TimeSlot.all
-    end
+     .joins(:time_slot_registrations)
+     .where('time_slot_registrations.filled = ?', false)
+     .where('time_slot_registrations.tasker_id = ?', params[:tasker_id])
 
     render 'api/time_slots/index'
   end
 end
+
+
+# if current_user.is_a? Client
+#   puts 'tasker time slots being rendered'
+#   @time_slots = TimeSlot.includes(:hour, :day)
+#   .joins(:time_slot_registrations)
+#   .where('time_slot_registrations.filled = ?', false)
+#   .where('time_slot_registrations.tasker_id = ?', params[:tasker_id])
+# else
+#   puts 'all time slots being rendered'
+#   @time_slots = TimeSlot.all
+# end
