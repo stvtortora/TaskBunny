e307245b8b2777ba19a5f8d4d53fbc44a3ed32e5
Steven <stvtortora@gmail.com>
2018-08-28 01:47:23 -0400

    modify tasks index method

--- app/controllers/api/time_slots_controller.rb
+++ app/controllers/api/time_slots_controller.rb
@@ -1,9 +1,14 @@
 class Api::TimeSlotsController < ApplicationController
   def index
-    @time_slots = TimeSlot.includes(:hour, :day)
-     .joins(:time_slot_registrations)
-     .where('time_slot_registrations.filled = ?', false)
-     .where('time_slot_registrations.tasker_id = ?', params[:tasker_id])
+    if !current_user
+      @time_slots = TimeSlot.includes(:hour, :day)
+       .joins(:time_slot_registrations)
+       .where('time_slot_registrations.filled = ?', false)
+       .where('time_slot_registrations.tasker_id = ?', params[:tasker_id])
+   else
+     puts 'all time slots being rendered'
+     @time_slots = TimeSlot.all
+   end
 
     render 'api/time_slots/index'
   end
